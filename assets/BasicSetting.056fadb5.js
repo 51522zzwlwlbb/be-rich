b40793645a4aa1233bef23a907259905824355ac
炒饭大师干饭达人恋爱废柴健身呆头鹅 <20981116+51522zzwlwlbb@users.noreply.github.com>
2024-07-31 17:53:05 +0800

    更新数据

---
+++ assets/BasicSetting.056fadb5.js
@@ -0,0 +1,1 @@
+import"./BasicSetting.vue_vue_type_script_setup_true_lang.2ca6fa9d.js";import{_ as t}from"./BasicSetting.vue_vue_type_script_setup_true_lang.2ca6fa9d.js";import"./index.fe2d9d98.js";export{t as default};
--- assets/list.645f7378.js
+++
@@ -1,1 +0,0 @@
-import{aB as e}from"./index.3c110038.js";function a(t){return e.request({url:"/table/list",method:"get",params:t})}export{a as g};
